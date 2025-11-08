19886457dc54f8e8c450117930ae8c30daf739ea
jasleen kaur <jjasleenkaur2@rrc.ca>
2025-11-08 15:59:24 -0600

    added Swagger configuration for Recipe Platform API as well as made changes in the app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import reciperoutes from "./api/v1/routes/reciperoutes";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./api/v1/swagger/swagger";
 
 const app = express();
-
 app.use(express.json());
 
 app.use("/api/v1/recipes", reciperoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 export default app;
 
